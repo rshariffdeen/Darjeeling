c1867f14573c906e978a667511a3dafdaa5c53f1
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2022-03-21 06:23:27 +0800

    change exit code with instrumentation

--- src/darjeeling/coverage/gcov.py
+++ src/darjeeling/coverage/gcov.py
@@ -30,7 +30,7 @@ _INSTRUMENTATION = (
     "void darjeeling_sighandler(int sig){\n"
     "  __gcov_flush();\n"
     "  if(sig != SIGUSR1 && sig != SIGUSR2)\n"
-    "    exit(1);\n"
+    "    exit(127);\n"
     "}\n"
     "void darjeeling_ctor (void) __attribute__ ((constructor));\n"
     "void darjeeling_ctor (void) {\n"
